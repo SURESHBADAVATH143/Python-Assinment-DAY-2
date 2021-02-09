2a93159b1184d8dcaee12ab8c245ba00d2847297
SURESH BADAVATH <78695828+SURESHBADAVATH143@users.noreply.github.com>
2021-02-09 18:49:10 +0530

    Add files via upload

---
+++ Day 1 Assinment/dict.py
@@ -0,0 +1,26 @@
+my_dict = {'name': 'Jack', 'age': 26}
+
+# Output: Jack
+print(my_dict['name'])
+
+# Output: 26
+print(my_dict.get('age'))
+
+# Trying to access keys which doesn't exist throws error
+# Output None
+print(my_dict.get('address'))
+
+# KeyError
+print(my_dict['address'])
+# Changing and adding Dictionary Elements
+my_dict = {'name': 'Jack', 'age': 26}
+
+# update value
+my_dict['age'] = 27
+
+#Output: {'age': 27, 'name': 'Jack'}
+print(my_dict)
+
+# add item
+my_dict['address'] = 'Downtown'
+print(my_dict)
---
+++ Day 1 Assinment/list.py
@@ -0,0 +1,5 @@
+# Adds List Element as value of List.
+List = ['Mathematics', 'chemistry', 1997, 2000]
+#List.append(10087)
+List.insert(2,1204)
+print(List)
